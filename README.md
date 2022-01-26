56784c59194a52f8bedc9e993c7f99a2acf67b2f
fedy0 <ifedioraelochukwuc@gmail.com>
2022-01-26 21:27:47 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rpc
-Remote Procedural Call for Embedded Interfaces
+Remote Procedure Call for Embedded Interfaces
+
+Remote Procedure Call is a software communication protocol that one program/system can use to request a service from a program/system located in another computer on a network (wired or wireless) without having to understand the network's details
---
+++ examples/rpc/rpc.ino
@@ -0,0 +1,72 @@
+// Remote Control - As The Controller Device
+
+#include <rpc.h>
+
+RPC::RPCSERIAL interface(RPC::HARDWARE, RPC::MASTER);
+
+
+// This example shows reading a Digital I/O pin remotely.
+void digital_read_example()
+{
+    uint8_t state;
+    if (interface.call_no_args(F("digital_read"), &state, sizeof(state)))
+    {
+        Serial.print(F("Remote Digital I/O State: "));
+        Serial.println(state);
+    }
+}
+
+// This example shows reading an Analog I/O pin remotely.
+void analog_read_example()
+{
+    uint16_t state;
+    if (interface.call_no_args(F("analog_read"), &state, sizeof(state)))
+    {
+        Serial.print(F("Remote Analog I/O State: "));
+        Serial.println(state);
+    }
+}
+
+// This example shows writing a Digital I/O pin remotely.
+void digital_write_example()
+{
+    static uint8_t state = 0;
+    if (interface.call(F("digital_write"), &state, sizeof(state)))
+    {
+        state = !state;
+    }
+}
+
+// This example shows writing an Analog I/O pin remotely.
+void analog_write_example()
+{
+    static uint8_t state = 0;
+    if (interface.call(F("digital_write"), &state, sizeof(state)))
+    {
+        state = state + 1;
+    }
+}
+
+// This example prints to the serial interface remotely.
+void serial_print_example()
+{
+  const char str[] = "Hello World!";
+  interface.call(F("serial_print"), (char*) str, strlen(str));
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.Println(F("Testing RPC: Remote Control - As The Controller Device"));
+    Serial1.begin(115200);
+    interface.begin(&Serial1);
+}
+
+void loop()
+{
+    digital_read_example();
+    analog_read_example();
+    digital_write_example();
+    analog_write_example();
+    serial_print_example();
+}
